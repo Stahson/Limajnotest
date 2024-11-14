c86ece468b42c4655e27e4d9df33434acbc6a540
Stahson <stasiek.gwiazda0@gmail.com>
2024-11-14 16:45:22 +0100

    first commit

---
+++ database.js
@@ -0,0 +1,22 @@
+// const firebaseConfig = {
+
+//     apiKey: "AIzaSyCS2J5AxvS9PpNXqNLNjKP4vM1jtGOjITQ",
+  
+//     authDomain: "flutter-a9dce.firebaseapp.com",
+  
+//     databaseURL: "https://flutter-a9dce-default-rtdb.firebaseio.com",
+  
+//     projectId: "flutter-a9dce",
+  
+//     storageBucket: "flutter-a9dce.firebasestorage.app",
+  
+//     messagingSenderId: "561921975040",
+  
+//     appId: "1:561921975040:web:f68ca5366233d71c0849e5",
+  
+//     measurementId: "G-0269001WYP"
+  
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// var db =firebase.database().ref('limajno');
